33bced6aa5e22e0684891d4c75609b68f8435ace
kgni <admin@mdia.dk>
2022-05-18 22:33:01 +0200

    create cards with props

--- scrimba/03-airbnb-clone/src/components/Cards/Card.jsx
+++ scrimba/03-airbnb-clone/src/components/Cards/Card.jsx
@@ -1,24 +1,24 @@
 import CardImage from '../../img/katie-zaferes.png';
 import StarIcon from '../../img/star.png';
 
-function Card() {
+function Card(props) {
 	return (
 		<div className="card">
-			<div className="tag">sold out</div>
+			<div className="tag">{props.tag}</div>
 			<img src={CardImage} alt="" />
 			<div className="card-rating">
 				<img src={StarIcon} alt="" />
-				<span className="rating">5.0</span>
-				<span className="rating-count">(6)</span>
+				<span className="rating">{props.rating}</span>
+				<span className="rating-count">({props.reviewCount})</span>
 				<span className="dot"></span>
-				<div className="country">USA</div>
+				<div className="country">{props.country}</div>
 			</div>
 			<div className="card-description">
-				<span>Life lessons with Katie Zaferes</span>
+				<span>{props.title}</span>
 			</div>
 			<div className="price">
 				<span>
-					From $136 <span className="person">/ person</span>
+					From ${props.price} <span className="person">/ person</span>
 				</span>
 			</div>
 		</div>
--- scrimba/03-airbnb-clone/src/components/Cards/CardSection.jsx
+++ scrimba/03-airbnb-clone/src/components/Cards/CardSection.jsx
@@ -1,13 +1,37 @@
 import Card from './Card';
+import KatieZaferesImg from '../../img/katie-zaferes.png';
 
 import './CardSection.css';
 
 function CardSection() {
 	return (
 		<section className="card-section">
-			<Card />
-			<Card />
-			<Card />
+			<Card
+				tag="sold out"
+				img={KatieZaferesImg}
+				rating="5.0"
+				reviewCount={6}
+				country="USA"
+				title="Life Lessons with Katie Zaferes"
+				price={136}
+			/>
+			<Card
+				tag="online"
+				img={KatieZaferesImg}
+				rating="5.0"
+				reviewCount={6}
+				country="USA"
+				title="Life Lessons with Katie Zaferes"
+				price={136}
+			/>
+			<Card
+				img={KatieZaferesImg}
+				rating="5.0"
+				reviewCount={6}
+				country="USA"
+				title="Life Lessons with Katie Zaferes"
+				price={136}
+			/>
 		</section>
 	);
 }
