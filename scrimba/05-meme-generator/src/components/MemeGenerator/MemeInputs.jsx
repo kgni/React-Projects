de061c31eea1a738f5463bde2e438ae07dd07b4a
kgni <admin@mdia.dk>
2022-05-22 19:51:53 +0200

    add state for generator random meme image

--- scrimba/05-meme-generator/src/components/MemeGenerator/MemeInputs.jsx
+++ scrimba/05-meme-generator/src/components/MemeGenerator/MemeInputs.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import './MemeInputs.css';
+import memeData from '../../memeData';
+
 const MemeInputs = () => {
+	const [memeImg, setMemeImg] = React.useState(memeData.data.memes[0].url);
+	function getRandMeme() {
+		const memesArray = memeData.data.memes;
+		const randNum = Math.floor(Math.random() * memesArray.length);
+		console.log(randNum);
+		const url = memesArray[randNum].url;
+		console.log(url);
+
+		setMemeImg(url);
+	}
 	return (
 		<>
-			<form action="/" method="GET" className="input-form">
+			<div action="/" method="GET" className="input-form">
 				<div className="inputs">
 					<input placeholder="Top text..." type="text" />
 					<input placeholder="Bottom text..." type="text" />
 				</div>
-				<button className="new-meme-btn">Get a new meme image</button>
-			</form>
+				<button onClick={getRandMeme} className="new-meme-btn">
+					Get a new meme image
+				</button>
+			</div>
 			<div className="meme-output">
-				<img src="" alt="" className="meme-output__img" />
+				<img src={memeImg} alt="" className="meme-output__img" />
 			</div>
 		</>
 	);
