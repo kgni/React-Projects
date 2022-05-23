7939de83e76f14d586370066fe4099872da690a7
kgni <admin@mdia.dk>
2022-05-23 10:31:48 +0200

    add random image when refreshing page, instead of hardcoding the first image

--- scrimba/05-meme-generator/src/components/MemeGenerator/MemeInputs.jsx
+++ scrimba/05-meme-generator/src/components/MemeGenerator/MemeInputs.jsx
@@ -3,14 +3,16 @@ import './MemeInputs.css';
 import memeData from '../../memeData';
 
 const MemeInputs = () => {
-	const [memeImg, setMemeImg] = React.useState(memeData.data.memes[0].url);
+	const memesArray = memeData.data.memes;
+	const randNum = Math.floor(Math.random() * memesArray.length);
+	const [memeImg, setMemeImg] = React.useState(
+		memeData.data.memes[randNum].url
+	);
 	function getRandMeme() {
-		const memesArray = memeData.data.memes;
 		const randNum = Math.floor(Math.random() * memesArray.length);
 		console.log(randNum);
 		const url = memesArray[randNum].url;
 		console.log(url);
-
 		setMemeImg(url);
 	}
 	return (
