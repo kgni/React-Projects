99ff52c2336f613b0f37448af0f2a0cf14f2c301
kgni <admin@mdia.dk>
2022-05-27 20:06:35 +0200

    add states to input and appending inputs to the DOM realtime

--- scrimba/05-meme-generator/src/components/MemeGenerator/MemeInputs.jsx
+++ scrimba/05-meme-generator/src/components/MemeGenerator/MemeInputs.jsx
@@ -13,6 +13,16 @@ const MemeInputs = () => {
 		randomImage: allMemeImages[randNum].url,
 	});
 
+	function handleChange(event) {
+		const { name, value } = event.target;
+		setMeme((prevData) => {
+			return {
+				...prevData,
+				[name]: value,
+			};
+		});
+	}
+
 	function getRandMeme() {
 		const randNum = Math.floor(Math.random() * allMemeImages.length);
 		const url = allMemeImages[randNum].url;
@@ -27,8 +37,20 @@ const MemeInputs = () => {
 		<>
 			<div action="/" method="GET" className="input-form">
 				<div className="inputs">
-					<input placeholder="Top text..." type="text" />
-					<input placeholder="Bottom text..." type="text" />
+					<input
+						onChange={handleChange}
+						value={meme.topText}
+						name="topText"
+						placeholder="Top text..."
+						type="text"
+					/>
+					<input
+						onChange={handleChange}
+						value={meme.bottomText}
+						name="bottomText"
+						placeholder="Bottom text..."
+						type="text"
+					/>
 				</div>
 				<button onClick={getRandMeme} className="new-meme-btn">
 					Get a new meme image
@@ -36,6 +58,8 @@ const MemeInputs = () => {
 			</div>
 			<div className="meme-output">
 				<img src={meme.randomImage} alt="" className="meme-output__img" />
+				<h2 className="meme--text top">{meme.topText}</h2>
+				<h2 className="meme--text bottom">{meme.bottomText}</h2>
 			</div>
 		</>
 	);
