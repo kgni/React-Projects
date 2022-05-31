510331d12b06f682e1713253e482afe4c2d0febd
kgni <admin@mdia.dk>
2022-05-31 22:44:56 +0200

    add roll dice button and functionality

--- scrimba/06-tenzies/src/App.js
+++ scrimba/06-tenzies/src/App.js
@@ -1,16 +1,28 @@
+import React, { useState, useEffect } from 'react';
 import Die from './modules/Die';
 
 export default function App() {
-	const dieArray = [];
+	const [dice, setDice] = useState(allNewDice());
 
-	for (let i = 0; i < 10; i++) {
-		const randNum = Math.floor(Math.random() * 6) + 1;
-		dieArray.push(<Die value={randNum} />);
+	function allNewDice() {
+		const newDice = [];
+		for (let i = 0; i < 10; i++) {
+			newDice.push(Math.ceil(Math.random() * 6));
+		}
+		return newDice;
+	}
+	const dieArray = dice.map((el) => <Die value={el} />);
+
+	function rollDice() {
+		setDice(allNewDice());
 	}
 
 	return (
 		<main>
 			<div className="die-container">{dieArray}</div>
+			<button onClick={rollDice} className="roll-btn">
+				Roll dice
+			</button>
 		</main>
 	);
 }
