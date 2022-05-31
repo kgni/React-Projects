e554a1babcda6336618cc2d319a587745d6cd0ed
kgni <admin@mdia.dk>
2022-05-31 21:24:23 +0200

    add Die component, rendering 10 into die-container

--- scrimba/06-tenzies/src/App.js
+++ scrimba/06-tenzies/src/App.js
@@ -1,3 +1,16 @@
+import Die from './modules/Die';
+
 export default function App() {
-	return <main></main>;
+	const dieArray = [];
+
+	for (let i = 0; i < 10; i++) {
+		const randNum = Math.floor(Math.random() * 6) + 1;
+		dieArray.push(<Die value={randNum} />);
+	}
+
+	return (
+		<main>
+			<div className="die-container">{dieArray}</div>
+		</main>
+	);
 }
