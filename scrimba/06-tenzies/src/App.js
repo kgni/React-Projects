47d85587cbc4f177d56fb2ee550eb00b49fef867
kgni <admin@mdia.dk>
2022-06-01 22:32:51 +0200

    add functionality to Tenzies game, all working now

--- scrimba/06-tenzies/src/App.js
+++ scrimba/06-tenzies/src/App.js
@@ -3,26 +3,98 @@ import Die from './modules/Die';
 
 export default function App() {
 	const [dice, setDice] = useState(allNewDice());
+	const [isGameOver, setIsGameOver] = useState(false);
 
 	function allNewDice() {
 		const newDice = [];
 		for (let i = 0; i < 10; i++) {
-			newDice.push(Math.ceil(Math.random() * 6));
+			const value = Math.ceil(Math.random() * 6);
+			newDice.push({
+				id: i + 1,
+				value: value,
+				isLocked: false,
+			});
 		}
 		return newDice;
 	}
-	const dieArray = dice.map((el) => <Die value={el} />);
+
+	function setIsLocked(id) {
+		setDice((oldDice) => {
+			return oldDice.map((dice) => {
+				if (dice.id === id) {
+					return {
+						...dice,
+						isLocked: !dice.isLocked,
+					};
+				} else {
+					return {
+						...dice,
+					};
+				}
+			});
+		});
+	}
 
 	function rollDice() {
-		setDice(allNewDice());
+		setDice((oldDice) => {
+			return oldDice.map((dice) => {
+				const value = Math.ceil(Math.random() * 6);
+				if (!dice.isLocked) {
+					return {
+						...dice,
+						value: value,
+					};
+				} else {
+					return {
+						...dice,
+					};
+				}
+			});
+		});
 	}
+
+	useEffect(() => {
+		if (
+			dice.every((el) => el.isLocked) &&
+			dice.every((el) => el.value === dice[0].value)
+		) {
+			setIsGameOver(true);
+		} else {
+			setIsGameOver(false);
+		}
+	}, [dice]);
+
+	function restartGame() {
+		const newDice = [];
+		for (let i = 0; i < 10; i++) {
+			const value = Math.ceil(Math.random() * 6);
+			newDice.push({
+				id: i + 1,
+				value: value,
+				isLocked: false,
+			});
+		}
+		setIsGameOver(false);
+		return newDice;
+	}
+
+	const dieArray = dice.map((el) => (
+		<Die {...el} setIsLocked={setIsLocked} key={el.id} />
+	));
 
 	return (
 		<main>
 			<div className="die-container">{dieArray}</div>
-			<button onClick={rollDice} className="roll-btn">
-				Roll dice
-			</button>
+
+			{isGameOver ? (
+				<button onClick={() => setDice(restartGame())} className="roll-btn">
+					Restart game
+				</button>
+			) : (
+				<button onClick={rollDice} className="roll-btn">
+					Roll dice
+				</button>
+			)}
 		</main>
 	);
 }
--- scrimba/06-tenzies/src/modules/Die.jsx
+++ scrimba/06-tenzies/src/modules/Die.jsx
@@ -1,5 +1,17 @@
 import './Die.css';
 
 export default function Die(props) {
-	return <div className="die">{props.value}</div>;
+	const checked = {
+		backgroundColor: '#99f2a2',
+	};
+
+	return (
+		<div
+			onClick={() => props.setIsLocked(props.id)}
+			className="die"
+			style={props.isLocked ? checked : { backgroundColor: 'white' }}
+		>
+			{props.value}
+		</div>
+	);
 }
