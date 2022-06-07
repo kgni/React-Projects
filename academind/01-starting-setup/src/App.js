89f897211a14fbcc87a534ca9271936928c033b2
kgni <admin@mdia.dk>
2022-06-07 23:35:30 +0200

    add state and onChange functions for all of our inputs

--- academind/01-starting-setup/src/App.js
+++ academind/01-starting-setup/src/App.js
@@ -1,15 +1,20 @@
 import ExpenseItem from './components/Expenses/ExpenseItem';
 import expenses from './data/expenses';
 import Card from './components/UI/Card';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
 	const expensesItems = expenses.map((item) => {
 		return <ExpenseItem key={item.id} {...item} />;
 	});
 
+	const addExpenseHandler = (expense) => {
+		console.log(expense);
+	};
+
 	return (
 		<div>
-			<h2>Let's get started!</h2>
+			<NewExpense onAddExpense={addExpenseHandler} />
 			<Card className="card-container">{expensesItems}</Card>
 		</div>
 	);
---
+++ academind/01-starting-setup/src/components/NewExpense/ExpenseForm.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import './ExpenseForm.css';
+
+const ExpenseForm = (props) => {
+	const [enteredTitle, setEnteredTitle] = useState('');
+	const [enteredAmount, setEnteredAmount] = useState('');
+	const [enteredDate, setEnteredDate] = useState('');
+
+	// you can also add every single value to 1 state in an object. Keep in mind, when updating this state using our setter function, you have to update the entire object, where you are spreading the entire object first, and the updating the values that needs to be updating.
+	const [formData, setFormData] = useState({
+		enteredTitle: '',
+		enteredAmount: '',
+		enteredDate: '',
+	});
+
+	const titleChangeHandler = (event) => {
+		setEnteredTitle(event.target.value);
+
+		setFormData((prevData) => {
+			return {
+				...prevData,
+				enteredTitle: event.target.value,
+			};
+		});
+	};
+
+	const amountChangeHandler = (event) => {
+		setEnteredAmount(event.target.value);
+
+		setFormData((prevData) => {
+			return {
+				...prevData,
+				enteredAmount: event.target.value,
+			};
+		});
+	};
+	const dateChangeHandler = (event) => {
+		setEnteredDate(event.target.value);
+
+		setFormData((prevData) => {
+			return {
+				...prevData,
+				enteredDate: event.target.value,
+			};
+		});
+	};
+
+	const submitHandler = (event) => {
+		event.preventDefault();
+
+		const expenseData = {
+			title: enteredTitle,
+			amount: enteredAmount,
+			date: new Date(enteredDate),
+		};
+
+		props.onSaveExpenseData(expenseData);
+
+		setEnteredTitle('');
+		setEnteredAmount('');
+		setEnteredDate('');
+	};
+
+	return (
+		<form onSubmit={submitHandler} action="">
+			<div className="new-expense__controls">
+				<div className="new-expense__control">
+					<label htmlFor="">Title</label>
+					<input
+						value={enteredTitle}
+						onChange={titleChangeHandler}
+						type="text"
+					/>
+				</div>
+				<div className="new-expense__control">
+					<label htmlFor="">Amount</label>
+					<input
+						value={enteredAmount}
+						onChange={amountChangeHandler}
+						type="number"
+						min="0.01"
+						step="0.01"
+					/>
+				</div>
+				<div className="new-expense__control">
+					<label htmlFor="">Date</label>
+					<input
+						value={enteredDate}
+						onChange={dateChangeHandler}
+						type="date"
+						min="2019-01-01"
+						max="2022-12-31"
+					/>
+				</div>
+			</div>
+			<div className="new-expense__actions">
+				<button type="submit">Add Expense</button>
+			</div>
+		</form>
+	);
+};
+
+export default ExpenseForm;
