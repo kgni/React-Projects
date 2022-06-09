70362db693448cbd94a78c6b31c58e5c82c8a9d6
kgni <admin@mdia.dk>
2022-06-09 16:11:26 +0200

    add filtering functionality to our expenses, lifted up state from input component to app component, now we are able to add new items

--- academind/01-starting-setup/src/App.js
+++ academind/01-starting-setup/src/App.js
@@ -1,20 +1,31 @@
+import React, { useState } from 'react';
 import ExpenseItem from './components/Expenses/ExpenseItem';
 import expenses from './data/expenses';
 import Card from './components/UI/Card';
 import NewExpense from './components/NewExpense/NewExpense';
+import ExpensesFilter from './components/Expenses/ExpensesFilter';
 
 function App() {
-	const expensesItems = expenses.map((item) => {
-		return <ExpenseItem key={item.id} {...item} />;
+	const [data, setData] = useState(expenses);
+	const [filterYear, setFilterYear] = useState('showAll');
+
+	const expensesItems = data.map((item) => {
+		if (
+			item.date.getFullYear().toString() === filterYear ||
+			filterYear === 'showAll'
+		) {
+			return <ExpenseItem key={item.id} {...item} />;
+		}
 	});
 
 	const addExpenseHandler = (expense) => {
-		console.log(expense);
+		setData((prevData) => [...prevData, expense]);
 	};
 
 	return (
-		<div>
+		<div className="app-container">
 			<NewExpense onAddExpense={addExpenseHandler} />
+			<ExpensesFilter setFilterYear={setFilterYear} />
 			<Card className="card-container">{expensesItems}</Card>
 		</div>
 	);
