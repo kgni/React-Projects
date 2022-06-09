3aa518173b40541e800352ed35eaeab5a66f8519
kgni <admin@mdia.dk>
2022-06-09 17:53:13 +0200

    refactor filtering of expenses, adding conditional renders if no expenses are added to a specific year

--- academind/01-starting-setup/src/App.js
+++ academind/01-starting-setup/src/App.js
@@ -1,33 +1,38 @@
 import React, { useState } from 'react';
 import ExpenseItem from './components/Expenses/ExpenseItem';
-import expenses from './data/expenses';
+import DUMMY_EXPENSES from './data/DUMMY_EXPENSES';
 import Card from './components/UI/Card';
 import NewExpense from './components/NewExpense/NewExpense';
 import ExpensesFilter from './components/Expenses/ExpensesFilter';
 
 function App() {
-	const [data, setData] = useState(expenses);
+	const [expensesData, setExpensesData] = useState(DUMMY_EXPENSES);
 	const [filterYear, setFilterYear] = useState('showAll');
 
-	const expensesItems = data.map((item) => {
+	const filteredExpenses = expensesData.filter((expense) => {
 		if (
-			item.date.getFullYear().toString() === filterYear ||
+			expense.date.getFullYear().toString() === filterYear ||
 			filterYear === 'showAll'
 		) {
-			return <ExpenseItem key={item.id} {...item} />;
+			return expense;
 		}
 	});
 
 	const addExpenseHandler = (expense) => {
-		setData((prevData) => [...prevData, expense]);
+		setExpensesData((prevExpenses) => [expense, ...prevExpenses]);
 	};
 
+	console.log(filteredExpenses.length);
 	return (
 		<div className="app-container">
 			<NewExpense onAddExpense={addExpenseHandler} />
 			<Card className="card-container">
 				<ExpensesFilter filterYear={filterYear} setFilterYear={setFilterYear} />
-				{expensesItems}
+				{filteredExpenses.length === 0 && <p>No expenses found.</p>}
+				{filteredExpenses.length > 0 &&
+					filteredExpenses.map((expense) => (
+						<ExpenseItem key={expense.id} {...expense} />
+					))}
 			</Card>
 		</div>
 	);
